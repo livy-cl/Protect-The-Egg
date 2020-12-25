33e693c40b850be4f516d881afcd997dd3e7c97a
Jan Cool <jan.joris.cool@gmail.com>
2020-12-26 00:08:21 +0100

    updated the website

--- src/networkManager.py
+++ src/networkManager.py
@@ -1,5 +1,5 @@
 def network_handler(socket, components: dict):
-    from log import debugging
+    from log import debugging, message
     request = socket.recv(1024)
     request = str(request)
     debugging('Socket content = {}'.format(request))
@@ -9,23 +9,25 @@ def network_handler(socket, components: dict):
     motor_right = request.find('/?moveMotor=right')
 
     if motor_off == 6:
-        debugging('Turn motor off')
+        message('Turn motor off')
         components["motor"]["object"].stop()
     elif motor_left == 6:
-        debugging("Move motor left")
+        message("Move motor left")
         components["motor"]["object"].forward(50)
     elif motor_right == 6:
-        debugging("Move motor right")
+        message("Move motor right")
         components["motor"]["object"].reverse(50)
 
     from miscellaneous import read_file
-    """
-    response = read_file("website/index.html")
-    response = response.format(laser="whoo", senser="sensen", led="ledy")
+    response = read_file("index.html").split("%%%%%%%")
+    response.insert(3, str(components["led"]["object"].value()))
+    response.insert(2, str(components["lightSensor"]["object"].read()))
+    response.insert(1, str(components["laser"]["object"].value()))
+    response = ''.join(map(str, response))
     response = response.encode('utf-8')
-    """
+
     socket.send('HTTP/1.1 200 OK\n'.encode('utf-8'))
     socket.send('Content-Type: text/html\n'.encode('utf-8'))
     socket.send('Connection: close\n\n'.encode('utf-8'))
-    socket.sendall(read_file("website/index.html"))
+    socket.sendall(response)
     socket.close()
